c5ce8c93087bf137158c90b892e666352f8653de
Ashish Maurya <ashishanshumaurya1572002@gmail.com>
2024-09-07 23:46:16 +0530

    change

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = 'http://localhost:4000/api/v1'
-export const BASE_URL = 'https://travelia-travel-and-tour-booking-website.onrender.com'
+export const BASE_URL = 'https://travelia-travel-and-tour-booking-website.onrender.com/api/v1'
 // export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
